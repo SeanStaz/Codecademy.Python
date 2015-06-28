2f0f3d977d553f76afd829c283f04fee50fa7eb6
Sean Staz <seanstaz52@gmail.com>
2015-06-28 15:45:43 -0500

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,7 +1,7 @@
-    "Define a function called is_prime that takes a number x as input.
-    For each number n from 2 to x - 1, test if x is evenly divisible by n.
-    If it is, return False.
-    If none of them are, then return True."
+    "Define a function called is_prime that takes a number x as input."
+    "For each number n from 2 to x - 1, test if x is evenly divisible by n."
+    "If it is, return False."
+    "If none of them are, then return True."
 
 
 def is_prime(x):
