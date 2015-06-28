bf2cc74d7164f66ecd41318c687768d2647aebfc
Sean Staz <seanstaz52@gmail.com>
2015-06-28 14:55:32 -0500

    Update digit_sum.py

--- digit_sum.py
+++ digit_sum.py
@@ -1,3 +1,9 @@
+"Write a function called digit_sum that takes a positive integer n as input and returns the sum of all that number's digits."
+
+"For example: digit_sum(1234) should return 10 which is 1 + 2 + 3 + 4."
+
+"(Assume that the number you are given will always be positive.)"
+
 def digit_sum(n):
     if n >= 0:
         sum = 0
