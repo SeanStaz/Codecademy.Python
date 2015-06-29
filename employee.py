261702e42cd9605f825fcff59750e8f7b9a5722e
Sean Staz <seanstaz52@gmail.com>
2015-06-29 15:33:59 -0500

    Create employee.py

---
+++ employee.py
@@ -0,0 +1,21 @@
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+        
+    def full_time_wage(self,others):
+        return super(PartTimeEmployee,self).calculate_wage(others)
+        
+milton = PartTimeEmployee("Milton")
+
+print milton.full_time_wage(10)
